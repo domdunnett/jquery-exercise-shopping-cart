a1fcf69449c1fb2094c2b837ce8e8515631bb432
Dominic Michael Joshua Dunnett <domdunnett@gmail.com>
2015-03-27 19:24:49 +0800

    restyle add row. refactor js code. DRY'd up total function into one. add bootstrap class to buttons. Change Total to update on on() rather than click() to account for dynamic elements

--- js/application.js
+++ js/application.js
@@ -1,46 +1,63 @@
 jQuery(document).ready(function() {
 
-	var totalPrice = Number($('#total-price').val());
+	// var totalPrice = 0;
 
   // Deletes item in shopping cart and updates total price on clicking delete button
-	$('.delete-button').click(function() {
-	  $(this).parent().parent().fadeOut();
-	  var itemPrice = Number($(this).parent().parent().find('.item-price').data('price'));
-	  var numOfItems = Number($(this).parent().find('.quantity').val());
-	  totalPrice -= itemPrice * numOfItems;
-	  $('#total-price').text('$'+totalPrice.toFixed(2));
+	$(document).on('click', '.delete-button', function() {
+	  
+	  var currentRow = $(this).parent().parent();
+
+	  currentRow.fadeOut(300, function() { 
+	  	$(this).remove(); 
+			calculateTotal();
+	  });
+
 	});
 
-	// At the moment this simply adds a row with the input from each 
+	// Dyanmically adds a new row in the smae style at the top of the list 
 	$('#add-button').click(function() {
 		var additionalItem = $('#add-item').val();
-		var additionalItemPrice = $('#add-price').val();
+		var additionalItemPrice = Number($('#add-price').val()).toFixed(2);
 	  $('#top-of-list').before(
 	      "<div class=\"row\"> \
 	        <div class=\"item-name col-xs-6\">"
 	          + additionalItem +
 	        "</div> \
-	        <div class=\"item-price col-xs-3\" data-price=\"10.00\">"
+	        <div class=\"item-price col-xs-3\" data-price=\"" + additionalItemPrice + "\">"
 	          + "$" + additionalItemPrice +
 	        "</div> \
 	        <div class=\"item-qty col-xs-3\"> \
 	          <label>QTY</label> \
 	          <input class=\"quantity\"> \
-	          <button class= \"delete-button\">Delete</button> \
+	          <button class= \"delete-button btn btn-default\">Delete</button> \
         </div>" 
 	  	);
 	});
 
 	// This updates the total price with every keystroke
-	$('.quantity').keyup(function() {
-		var itemPrice = Number($(this).parent().parent().find('.item-price').data('price'));
-		var numOfItems = Number($(this).val());
-		var newPrice = numOfItems * itemPrice;
-		totalPrice += newPrice;
+	$(document).on('keyup', '.quantity', function() {
+
+		calculateTotal();
+
+  });
+
+  //This function calculates the total of the quantities input in the input fields
+	function calculateTotal() {
+		
+		var priceArray = $('.item-price');
+		var quantityArray = $('.quantity');
+		var totalPrice = 0;
+		var priceOfItems;
+
+		for (var i = 0; i < priceArray.length; i++) {
+			var item = Number($(priceArray[i]).data('price'));
+			var quantity = Number($(quantityArray[i]).val());
+			priceOfItems = item * quantity;	
+			totalPrice += priceOfItems;
+		}
+
 		$('#total-price').text('$'+totalPrice.toFixed(2));
-	});
-
-
-
+		return totalPrice;
+	}
 
 });
